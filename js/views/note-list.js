d05addc5f99e000c648667c9d11cc68ff0a49b16
matthewbeta <hello@matthew-jackson.com>
2014-01-02 23:52:06 +0000

    Updated stylign. Added some functionality when no notes. Added link to menu and to write new note.

--- js/views/note-list.js
+++ js/views/note-list.js
@@ -11,11 +11,19 @@ var NoteListView = Backbone.View.extend({
 	},
 
 	render: function() {
+		var noNotes = "<p class='no-notes'>You've got no notes. <a href='/#note'>Get writing!</a>";
+				
 		this.$el.html('');
-		this.collection.each(function(note) {
+
+		if (this.collection.length > 0) {
+			this.collection.each(function(note) {
 			var noteSnippetView = new NoteSnippetView({ model: note});
 			this.$el.append(noteSnippetView.render().el);
 		}, this);
+		} else {
+			this.$el.append(noNotes)
+		}
+		
 		return this;
 	}
 
